96c6ed8d8aeb6f00be516ed0d8ad844c454a48f4
Matías Arno <amatiasarno@gmail.com>
2022-09-28 16:39:50 -0300

    Contenido de Contentful actualizado. Sección HOME administrada correctamente desde CMS

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+function fillHomePageContent(data) {
+
+    const homeTitleEl = document.querySelector(".home__title");
+    const homeSubtitleEl = document.querySelector(".home__subtitle");
+    const homeEl = document.querySelector(".home");
+
+    homeEl.style.backgroundImage = `url("${data.includes.Asset[1].fields.file.url}")`;
+    homeTitleEl.innerHTML = data.items[0].fields.title;
+    homeSubtitleEl.innerHTML = data.items[0].fields.subtitle;
+}
+
+function getHomePageInfo() {
+
+    return fetch("https://cdn.contentful.com/spaces/sezd397y3ob3/environments/master/entries?access_token=d30jfPjygQpzALTd6rttnxuBwCL9oUdLRc2L_fSzyiQ")
+        .then( resp => resp.json())
+        .then( data => data);
+}
+
 function main() {
+
+    getHomePageInfo().then( data => fillHomePageContent(data));
     
     const menuEl = document.querySelector(".menu");
     const contactEl = document.querySelector(".contact");
