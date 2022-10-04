ae99fb0789ec7a914e608af9f07d06e1745e082b
Matías Arno <amatiasarno@gmail.com>
2022-10-04 11:41:20 -0300

    Envio de formulario funcionando correctamente.

--- contact/index.js
+++ contact/index.js
@@ -13,6 +13,31 @@ function createContact(contactEl) {
         <textarea name="message" id="message" resize="none" class="contact__inputs contact__inputs--textarea"></textarea>
 
         <input type="submit" value="Send" class="contact__form-button">
-    </form>
-    `;
+    </form>`;
+
+    const formEl = document.querySelector(".contact__form");
+    
+    formEl.addEventListener("submit", (event) => {
+
+        event.preventDefault();
+
+        const url = "https://apx-api.vercel.app/api/utils/dwf";
+
+        const data = {
+            "to":"amatiasarno@gmail.com",
+            "message":`Message from ${formEl.name.value}:
+                ${formEl.message.value}`
+        }
+
+        const options = {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "content-type": "application/json" }
+        };
+
+        fetch(url, options)
+            .then( res => res.json())
+            .catch( error => console.error('Error:', error))
+            .then( res => console.log("The message has been sent!", res));
+    });
 }
